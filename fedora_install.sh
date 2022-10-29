62495e4382466190492c4ef481b60f6ef87a9deb
soboka <46482674+soboka@users.noreply.github.com>
2022-10-29 13:05:27 +0300

    Update fedora_install.sh

--- fedora_install.sh
+++ fedora_install.sh
@@ -1,1 +1,6 @@
-sudo dnf install
+#!/bin/sh
+
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+sudo dnf install alacritty btop deluge-gtk deadbeef deadbeef-plugins deadbeef-mpris2-plugin discord dunst ffmpegthumbnailer fish flameshot krita mediainfo mpv neofetch neovim pcmanfm picom rofi
