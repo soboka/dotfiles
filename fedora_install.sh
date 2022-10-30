f30a493d0cd9c28012838ed7b767208c776e6e81
soboka <46482674+soboka@users.noreply.github.com>
2022-10-30 22:54:39 +0200

    Update fedora_install.sh

--- fedora_install.sh
+++ fedora_install.sh
@@ -7,27 +7,35 @@ sudo echo 'install_weak_deps=False' | sudo tee -a /etc/dnf/dnf.conf &&
 sudo echo 'defaultyes=True' | sudo tee -a /etc/dnf/dnf.conf &&
 
 # enable auto trim
-sudo systemctl enable fstrim.timer
+sudo systemctl enable fstrim.timer &&
 
 # enable rpm fusion free and non-free
 sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm &&
+sudo dnf upgrade --refresh &&
 
 # enable flathub
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo &&
-flatpak update &&
-
-# install general softwares
-sudo dnf install -y alacritty btop deluge-gtk deadbeef deadbeef-plugins deadbeef-mpris2-plugin discord dunst ffmpegthumbnailer fish flameshot krita mediainfo mpv neofetch neovim pcmanfm picom rofi starship sxiv xed zathura &&
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo &&
+sudo flatpak update &&
 
 # install nvidia
 sudo dnf install -y akmod-nvidia &&
 sudo dnf install -y xorg-x11-drv-nvidia-cuda &&
 sudo dnf install -y vdpauinfo libva-vdpau-driver libva-utils vulkan &&
 
+# install general softwares
+sudo dnf install -y alacritty btop deluge-gtk deadbeef deadbeef-plugins deadbeef-mpris2-plugin discord dunst ffmpegthumbnailer fish flameshot mediainfo mpv neofetch neovim pcmanfm picom qalculate-gtk rofi starship sxiv xed zathura &&
+sudo flatpak install flathub com.bitwarden.desktop com.vscodium.codium &&
+sudo dnf install -y unzip p7zip p7zip-plugins unrar &&
+sudo dnf install -y gimp gimpfx-foundry gmic-gimp gimp-paint-studio krita libreoffice-calc libreoffice-draw libreoffice-langpack-hu libreoffice-writer thunderbird &&
+
 # install codecs
 sudo dnf install -y gstreamer1-plugins-{bad-*,good-*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel &&
 sudo dnf install -y lame* --exclude=lame-devel &&
 sudo dnf group upgrade -y --with-optional Multimedia &&
+
+# for gaming
+sudo dnf install wine lutris steam &&
+sudo flatpak install flathub net.davidotek.pupgui2 com.heroicgameslauncher.hgl &&
 
 # kvm virtualization setup
 sudo dnf install -y bridge-utils libvirt virt-install qemu-kvm &&
