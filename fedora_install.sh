693f16be03016b6f12ac9e7b9e5b59ae2d5f77fd
soboka <46482674+soboka@users.noreply.github.com>
2022-10-30 22:06:18 +0200

    Update fedora_install.sh

--- fedora_install.sh
+++ fedora_install.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 # configure dnf
-sudo echo defaultyes=True \ndeltarpm=True \nmax_parallel_downloads=6 \ninstall_weak_deps=False /etc/dnf/dnf.conf &&
+sudo echo 'max_parallel_downloads=6' | sudo tee -a /etc/dnf/dnf.conf &&
+sudo echo 'deltarpm=True' | sudo tee -a /etc/dnf/dnf.conf &&
+sudo echo 'install_weak_deps=False' | sudo tee -a /etc/dnf/dnf.conf &&
+sudo echo 'defaultyes=True' | sudo tee -a /etc/dnf/dnf.conf &&
+
+# enable auto trim
+sudo systemctl enable fstrim.timer
 
 # enable rpm fusion free and non-free
 sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm &&
@@ -28,3 +34,15 @@ sudo dnf install -y bridge-utils libvirt virt-install qemu-kvm &&
 sudo dnf install -y libvirt-devel virt-top libguestfs-tools guestfs-tools &&
 sudo systemctl enable libvirtd &&
 sudo dnf install -y virt-manager &&
+
+# qtile from source
+sudo dnf install -y python3-pip python3-cffi python3-xcffib pango wlroots-devel &&
+sudo dnf install -y python3-cairocffi &&
+sudo pip install --no-input qtile &&
+sudo echo '[Desktop Entry]' | sudo tee -a /usr/share/xsessions/qtile.desktop &&
+sudo echo 'Name=Qtile' | sudo tee -a /usr/share/xsessions/qtile.desktop &&
+sudo echo 'Comment=Qtile Session' | sudo tee -a /usr/share/xsessions/qtile.desktop &&
+sudo echo 'Exec=qtile start' | sudo tee -a /usr/share/xsessions/qtile.desktop &&
+sudo echo 'Type=Application' | sudo tee -a /usr/share/xsessions/qtile.desktop &&
+sudo echo 'Keywords=wm;tiling' | sudo tee -a /usr/share/xsessions/qtile.desktop &&
+sudo chmod +x /usr/share/xsessions/qtile.desktop &&
